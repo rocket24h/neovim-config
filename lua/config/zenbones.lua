1198141b6793b88c6bb63b0f3d6ed85927ba250e
Phan Huy Đức Tài <phanhuyductai@gmail.com>
2024-04-24 08:58:23 +0700

    Added zenbones customization

--- lua/config/zenbones.lua
+++ lua/config/zenbones.lua
@@ -4,5 +4,11 @@ vim.cmd("hi NvimTreeGitNewIcon guifg=#8BAE68")
 vim.cmd("hi NvimTreeGitDirtyIcon guifg=#B77E64")
 vim.cmd("hi NvimTreeGitStagedIcon guifg=#66A5AD")
 
-vim.o.solid_float_border = true
+vim.g.zenbones = {
+	solid_float_border = true,
+	-- transparent_background = true,
+	--	darkness = "warm",
+}
+vim.cmd("set termguicolors")
+vim.cmd("set background=dark")
 vim.cmd("colorscheme zenbones")
